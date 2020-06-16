4a052e934ed40120a8fe6a2276fa65cad04d023c
nanaobeng <nanaobengmarnu@gmail.com>
2020-06-16 19:38:51 +0000

    admin dashboard
    added an admin dasboard interface mock up

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
